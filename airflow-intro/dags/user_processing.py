14ca5f395f21830da70ea3cae29be7c9b9e26953
renan muniz <renan.mmerlo@gmail.com>
2025-11-19 11:00:17 -0300

    Apache AirFlow - Until class 30

--- airflow-intro/dags/user_processing.py
+++ airflow-intro/dags/user_processing.py
@@ -1,14 +1,15 @@
 from airflow.sdk import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.sdk.bases.sensor import PokeReturnValue
-
+    
 @dag
 def user_processing():
+    
     create_table = SQLExecuteQueryOperator(
         task_id="create_table",
         conn_id="postgres",
         sql="""
-            CREATE TABLE IF NOT EXISTS users (
+        CREATE TABLE IF NOT EXISTS users (
             id INT PRIMARY KEY,
             firstname VARCHAR(255),
             lastname VARCHAR(255),
@@ -17,7 +18,7 @@ def user_processing():
         )
         """
     )
-
+    
     @task.sensor(poke_interval=30, timeout=300)
     def is_api_available() -> PokeReturnValue:
         import requests
@@ -29,9 +30,29 @@ def user_processing():
         else:
             condition = False
             fake_user = None
-        return PokeReturnValue(is_done=condition, xcom_value=fake_user) 
+        return PokeReturnValue(is_done=condition, xcom_value=fake_user)
     
-    is_api_available()       
+    @task
+    def extract_user(fake_user):
+        return {
+            "id": fake_user["id"],
+            "firstname": fake_user["personalInfo"]["firstName"],
+            "lastname": fake_user["personalInfo"]["lastName"],
+            "email": fake_user["personalInfo"]["email"],
+    }
 
+    @task
+    def process_user(user_info):
+        import csv
 
+        with open("/tmp/user_info.csv", "w", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=user_info.keys())
+            writer.writeheader()
+            writer.writerow(user_info)
+
+    
+    fake_user = is_api_available()
+    user_info = extract_user(fake_user)
+    process_user(user_info)
+            
 user_processing()
